4427c77075be00bd02f2f9ced97136e59f422323
RickyBui <buiricky1996@gmail.com>
2023-09-27 13:31:13 -0500

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,17 @@
+#pragma once
+// degree.h
+// C867 Performance Assessment
+// Ricky Bui
+// 04/11/2023
+
+#ifndef degree_h
+#define degree_h
+#include<iostream>
+using namespace std;
+
+// Array used to print text from enum values
+
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+
+#endif // degree_h
